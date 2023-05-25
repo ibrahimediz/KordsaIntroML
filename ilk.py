bbf4a2dbc4056ae9df1d9c36f6bfc87df6027a8f
İbrahim EDİZ <edizdesign@gmail.com>
2023-05-25 15:47:53 +0000

    25052023

--- ilk.py
+++ ilk.py
@@ -4,3 +4,13 @@ filename = "pandas1"
 for item in liste:
     os.mkdir(f"/workspace/KordsaIntroML/Exercises/{item}")
     open(f"Exercises/{item}/{filename}.ipynb","wb+")
+
+
+
+""" 
+   Tip         kilo    fiyat
+0  elma         10      12
+1  armut        25      16
+2  portakal     30      12
+3  mandalina    12      35
+"""
