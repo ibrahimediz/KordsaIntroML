f0e4830ed7d204f817fc863814f2939b8310f441
İbrahim EDİZ <edizdesign@gmail.com>
2023-06-08 15:47:59 +0000

    08062023_1

--- ilk.py
+++ ilk.py
@@ -1,8 +1,8 @@
 liste = ["Demhat","EmreS","EmreG","Durmus","Cevaplar"]
 import os
-filename = "pandas1"
+filename = "outlier"
 for item in liste:
-    os.mkdir(f"/workspace/KordsaIntroML/Exercises/{item}")
+    # os.mkdir(f"/workspace/KordsaIntroML/Exercises/{item}")
     open(f"Exercises/{item}/{filename}.ipynb","wb+")
 
 
